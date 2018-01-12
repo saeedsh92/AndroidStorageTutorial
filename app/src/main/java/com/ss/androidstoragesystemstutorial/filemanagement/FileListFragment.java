ee72ed1e67e310ad125cbf1e1c8cf7031dc35e42
saeed shahini <saeedshahiniit@gmail.com>
2018-01-12 14:37:10 +0330

    Before Session 24-File Management

--- app/src/main/java/com/ss/androidstoragesystemstutorial/filemanagement/FileListFragment.java
+++ app/src/main/java/com/ss/androidstoragesystemstutorial/filemanagement/FileListFragment.java
@@ -18,10 +18,12 @@ import java.io.File;
  * Created by Saeed shahini on 1/12/2018.
  */
 
-public class FileListFragment extends Fragment implements FileAdapter.FileViewHolder.OnFileClick{
+public class FileListFragment extends Fragment implements FileAdapter.FileViewHolder.OnFileClick {
+    private static final String EXTRA_KEY_PATH = "path";
     private View rootView;
     private RecyclerView recyclerView;
-    private FileAdapter fileAdapter=new FileAdapter(this);
+    private FileAdapter fileAdapter = new FileAdapter(this);
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -33,13 +35,26 @@ public class FileListFragment extends Fragment implements FileAdapter.FileViewHo
     }
 
     private void setupViews() {
-        recyclerView=rootView.findViewById(R.id.rv_filesList);
-        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),2, LinearLayoutManager.VERTICAL,false));
+        recyclerView = rootView.findViewById(R.id.rv_filesList);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2, LinearLayoutManager.VERTICAL, false));
         recyclerView.setAdapter(fileAdapter);
+    }
+
+    public static FileListFragment newInstance(String path) {
+
+        Bundle args = new Bundle();
+        args.putString("path", path);
+        FileListFragment fragment = new FileListFragment();
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public void onFileClick(File file) {
 
+    }
+
+    public FileManagementActivity getParentActivity(){
+        return (FileManagementActivity) getActivity();
     }
 }
--- app/src/main/java/com/ss/androidstoragesystemstutorial/filemanagement/FileManagementActivity.java
+++ app/src/main/java/com/ss/androidstoragesystemstutorial/filemanagement/FileManagementActivity.java
@@ -1,7 +1,10 @@
 package com.ss.androidstoragesystemstutorial.filemanagement;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.ss.androidstoragesystemstutorial.R;
 
@@ -11,5 +14,19 @@ public class FileManagementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_managment);
+        Toolbar toolbar = findViewById(R.id.toolbar_fileManagement);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.frame_fileManagement_fragmentContainer,FileListFragment.newInstance(""));
+        fragmentTransaction.commit();
     }
 }
